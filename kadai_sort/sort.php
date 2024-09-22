beb17ff67f671407bf51f48c5e5d8edb7e9d7af2
tomita1229 <164143262+tomita1229@users.noreply.github.com>
2024-09-22 19:57:34 +0900

    kadai_sortをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+    <?php
+    function sort_2way($array, $order) {
+        if ($order) {
+            echo '昇順にソートします。<br>';
+            sort($array);
+        } else {
+            echo '降順にソートします。<br>';
+            rsort($array);
+        }
+        foreach ($array as $num) {
+            echo $num . '<br>';
+        }
+    }
+
+    $array = [15, 4, 18, 23, 10];
+    
+    sort_2way($array, TRUE);
+    sort_2way($array, FALSE);
+    ?>
+    </p>
+</body>
+
+</html>
